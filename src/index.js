b6dbfc712f8144d3b3c52f9a20a5ec6ef71befaf
“viettai” <viettaixca123@github.com>
2022-11-21 14:47:13 +0700

    Template engine

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require này sẽ chạy tới thư mục `node_modules` 
+    // => để tìm frameworks exoress.
+    const path = require('path');
+const express = require('express');
+const {engine: handlebars} = require('express-handlebars')
+const morgan = require('morgan');;
+const app = express();
+// port này là cổng httls địa chỉ để hiện trình duyệt.
+const port = 3000
+// Method get dùng để get path `/` 
+app.use(morgan('combined'))
+// templates engine ( mẫu động cơ)
+app.engine('.hbs', handlebars({
+  extname:".hbs"
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+// listen => gửi lắng nghe và gửi yêu cầu và hiện 
+// giao diện web
+app.listen(port, () => {
+  console.log(`Example app listening on port localhost:${port}`)
+})
